a7820708fc086d68ef86c039e882950aa88fd63c
Ali Shooshtari <shooshtariali05@gmail.com>
2021-01-24 15:50:33 +0330

    Modal

--- script.js
+++ script.js
@@ -5,21 +5,19 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnOpenModal);
+const openModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnOpenModal.length; i++)
-    btnOpenModal[i].addEventListener('click', function() {
-        console.log('Butten clicked');
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    });
+    btnOpenModal[i].addEventListener('click', openModal);
 
-btnCloseModal.addEventListener('click', function() {
+const closeModal = function() {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
-});
+};
 
-overlay.addEventListener('click', function() {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
